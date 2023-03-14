270e0976ec589c3ec65319d12dc8496e4d216049
João Simões <jarpsimoes@gmail.com>
2023-03-14 17:10:47 +0000

    FIX: Network peerings

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -83,6 +83,7 @@ module "vnet_shr" {
     linux_nsg_id = module.nsg_shared.shared_nsg.id
     windows_nsg_id = module.nsg_shared.shared_nsg.id
 }
+/*
 resource "azurerm_virtual_network_peering" "dev2share" {
   name = "peer-dev2share"
   resource_group_name = var.resource_group_name
@@ -90,20 +91,29 @@ resource "azurerm_virtual_network_peering" "dev2share" {
   remote_virtual_network_id = module.vnet_shr.vnet.id
 }
 resource "azurerm_virtual_network_peering" "share2dev" {
+  depends_on = [
+    azurerm_virtual_network_peering.dev2share
+  ]
   name = "peer-share2share"
   resource_group_name = var.resource_group_name
   virtual_network_name = module.vnet_shr.vnet.name
   remote_virtual_network_id = module.vnet_dev.vnet.id
 }
 resource "azurerm_virtual_network_peering" "prd2share" {
+  depends_on = [
+    azurerm_virtual_network_peering.share2dev
+  ]
   name = "peer-prd2share"
   resource_group_name = var.resource_group_name
   virtual_network_name = module.vnet_prd.vnet.name
   remote_virtual_network_id = module.vnet_shr.vnet.id
 }
 resource "azurerm_virtual_network_peering" "share2prd" {
+  deplends_on = [
+    azurerm_virtual_network_peering.prd2share
+  ]
   name = "peer-share2prd"
   resource_group_name = var.resource_group_name
   virtual_network_name = module.vnet_shr.vnet.name
   remote_virtual_network_id = module.vnet_prd.vnet.id
-}
+}*/
